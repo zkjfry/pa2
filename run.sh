ead386502e6ec0dd7b1e50ab5b24b1b4fa97a105
zkjfry <jeffreychen0826@gmail.com>
2020-11-28 18:38:50 +0800

    adding 5

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ modprobe tcp_probe
 # make sure we don't use a cached cwnd
 sysctl -w net.ipv4.tcp_no_metrics_save=1
 
-for qsize in 20 100; do
+for qsize in 5 20 100; do
     dir=bb-q$qsize
 
     python bufferbloat.py --dir=$dir --time=$time --bw-net=$bwnet --delay=$delay --maxq=$qsize
